fa7835cda034b68a87f8fde08a3de7a946859b34
Charles Martin <chasrmartin@gmail.com>
2017-11-14 13:11:25 -0700

    First draft of "why Hello world is important."

---
+++ YourVeryFirstProgram/Hello.java
@@ -0,0 +1,10 @@
+  /**
+   * Canonical hello world in Java. Note this *must* be in a file named
+   * 'Hello.java'
+   */
+  public class Hello {
+      public static void main(String[] argv){
+          System.out.println("Hello, world!");
+      }
+  }
+
